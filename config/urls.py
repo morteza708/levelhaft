19e28671a8a4ac72e448d99f332b5a6421d92262
KAKASHI <kamalian708@yahoo.com>
2025-07-09 13:43:27 +0330

    create page for erorrs 400,404,403,500,erorr test

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.shortcuts import render
 from products.views import upload_file
 
 admin.site.site_header = settings.ADMIN_SITE_HEADER
 admin.site.site_title = settings.ADMIN_SITE_TITLE
 admin.site.index_title = settings.ADMIN_INDEX_TITLE 
+
+# Test views for error pages
+def test_500(request):
+    return render(request, '500.html')
+
+def test_403(request):
+    return render(request, '403.html')
+
+def test_400(request):
+    return render(request, '400.html')
+
+def test_error_pages(request):
+    return render(request, 'error_test.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,6 +50,12 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('wallet/', include('wallet.urls', namespace='wallet')),
+    
+    # Test URLs for error pages (only in development)
+    path('test-errors/', test_error_pages, name='test_errors'),
+    path('test-500/', test_500, name='test_500'),
+    path('test-403/', test_403, name='test_403'),
+    path('test-400/', test_400, name='test_400'),
 ]
 urlpatterns += [
     path("ckeditor5/", include('django_ckeditor_5.urls')),
